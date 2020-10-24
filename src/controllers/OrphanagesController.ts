e3cbc79f65fcf4c682c9539f46d2f526cd8ff375
Gabriel Mendes Camargo de Barros <gmendescamargodebarros@gmail.com>
2020-10-24 13:57:21 -0300

    enviando dados ao banco de dados

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -4,101 +4,121 @@ import Orphanage from '../models/Orphanage';
 import orphanageView from '../views/orphanages_view';
 import * as Yup from 'yup';
 import User from '../models/User';
+import bcrypt from 'bcrypt';
 
 export default {
-    async index(request: Request, response: Response) {
-        const orphanagesRepository = getRepository(Orphanage);
-        
-        const orphanages = await orphanagesRepository.find({
-            relations: ['images']
-        });
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
 
-        return response.json(orphanageView.renderMany(orphanages));
-    },
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
 
-    async show(request: Request, response: Response) {
-        const { id } = request.params;
+    return response.json(orphanageView.renderMany(orphanages));
+  },
 
-        const orphanagesRepository = getRepository(Orphanage);
-        
-        const orphanage = await orphanagesRepository.findOneOrFail(id, {
-            relations: ['images']
-        });
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
 
-        return response.json(orphanageView.render(orphanage));
-    },
+    const orphanagesRepository = getRepository(Orphanage);
 
-    async create(request: Request, response: Response) {
-        const {
-            name,
-            latitude,
-            longitude,
-            about,
-            instructions,
-            opening_hours,
-            open_on_weekends,
-        } = request.body;
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
+
+    return response.json(orphanageView.render(orphanage));
+  },
+
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const requestImages = request.files as Express.Multer.File[];
+
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    });
+
+    const data = {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends: open_on_weekends == 'true',
+      images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
+    });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const orphanage = orphanagesRepository.create(data);
+
+    await orphanagesRepository.save(orphanage);
+
+    return response.status(201).json(orphanage);
+  },
+
+  async indexAccess(request: Request, response: Response) {
+    const {
+      name,
+      email,
+      pass,
+    } = request.body;
+
+    const userRepository = getRepository(User);
+
+    // await bcrypt.hash(pass, 10, (err: Error, hash) => {
+      
+    // });
+
+    const data = {
+      name,
+      email,
+      pass,
+    };
+
+    const user = userRepository.create(data);
+
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      pass: Yup.string().required(),
+    });
     
-        const orphanagesRepository = getRepository(Orphanage);
+    await schema.validate(data, {
+      abortEarly: false,
+    });
 
-        const requestImages = request.files as Express.Multer.File[];
+    await userRepository.save(user);
 
-        const images = requestImages.map(image => {
-            return { path: image.filename }
-        });
-
-        const data = {
-            name,
-            latitude,
-            longitude,
-            about,
-            instructions,
-            opening_hours,
-            open_on_weekends: open_on_weekends == 'true',
-            images
-        };
-
-        const schema = Yup.object().shape({
-            name: Yup.string().required(),
-            latitude: Yup.number().required(),
-            longitude: Yup.number().required(),
-            about: Yup.string().required().max(300),
-            instructions: Yup.string().required(),
-            opening_hours: Yup.string().required(),
-            open_on_weekends: Yup.boolean().required(),
-            images: Yup.array(
-                Yup.object().shape({
-                    path: Yup.string().required()
-                })
-            )
-        });
-
-        await schema.validate(data, {
-            abortEarly: false,
-        });
-    
-        const orphanage = orphanagesRepository.create(data);
-    
-        await orphanagesRepository.save(orphanage);
-    
-        return response.status(201).json(orphanage);
-    },
-
-    async indexAccess(request: Request, response: Response) {
-        const {
-            name,
-            email,
-            pass,
-        } = request.body;
-
-        const data = {
-            name,
-            email,
-            pass,
-        };
-
-        console.log(data);
-
-        return response.status(201).json(user);
-    },
+    return response.status(201).json(user);
+  },
 };
